0ea857f4a7f5e05c8a6a5837c8ceb7a731ba6cea
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-04-06 13:20:44 +0530

    Create User Model Class

---
+++ lib/model/user_model.dart
@@ -0,0 +1,31 @@
+
+
+class UserModel{
+  final int id;
+  final String name;
+  final int age;
+
+  // constructor
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.age
+});
+
+  factory UserModel.fromMap(Map<String,dynamic>map){
+    return UserModel(
+        id: map['id'],
+        name: map['name'],
+        age: map['age']
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      "id" : id,
+      "name" : name,
+      "age" : age
+    };
+  }
+
+}
